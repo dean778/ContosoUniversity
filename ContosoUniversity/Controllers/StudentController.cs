22c214daa0d9d8520f283b41194d877c4b9fa213
dean <dean778@hotmail.com>
2012-02-10 09:28:00 +0800

    aa

---
+++ ContosoUniversity/Controllers/StudentController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ContosoUniversity.Models;
+using System.Data;
+
+namespace ContosoUniversity.Controllers
+{
+    public class StudentController : Controller
+    {
+        //
+        // GET: /Student/
+        SchoolContext db = new SchoolContext();
+        public ViewResult Index(string sortOrder, string searchString)
+        {
+            ViewBag.NameSortParm = string.IsNullOrEmpty(sortOrder)?"Name desc":"";
+            ViewBag.DateSortParm = (sortOrder == "Date" ? "Date desc" : "Date");
+            var students = from s in db.Students select s;
+            switch (sortOrder)
+            {
+                case "Name desc":
+                    students = students.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    students = students.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "Date desc":
+                    students = students.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default:
+                    students = students.OrderBy(s => s.LastName);
+                    break; 
+
+            }
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                students = students.Where(s => s.LastName.ToUpper().Contains(searchString.ToUpper())
+                                       || s.FirstMidName.ToUpper().Contains(searchString.ToUpper()));
+            }
+
+            return View(students.ToList()); 
+        }
+
+        //
+        // GET: /Student/Details/5
+
+        public ViewResult Details(int id=0)
+        {
+            Student student = db.Students.Find(id);
+            return View(student);
+        }
+
+        //
+        // GET: /Student/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Student/Create
+
+        [HttpPost]
+        public ActionResult Create(Student student)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Students.Add(student);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DataException)
+            {
+                //Log the error (add a variable name after DataException) 
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+            return View(student); 
+
+        }
+        
+        //
+        // GET: /Student/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            Student student = db.Students.Find(id);
+            return View(student);
+        }
+
+        //
+        // POST: /Student/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Student student)
+        {
+
+            try
+            {
+                // TODO: Add update logic here
+                if (ModelState.IsValid)
+                {
+                    db.Entry(student).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(student);
+            }
+            catch
+            {
+                return View(student);
+            }
+
+        }
+
+        //
+        // GET: /Student/Delete/5
+ 
+        public ActionResult Delete(int id)
+        {
+            Student student = db.Students.Find(id);
+            return View(student);
+        }
+
+        //
+        // POST: /Student/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                Student student = db.Students.Find(id);
+                db.Students.Remove(student);
+                db.SaveChanges();
+            }
+            catch (DataException)
+            {
+                //Log the error (add a variable name after DataException) 
+                return RedirectToAction("Delete",
+                    new System.Web.Routing.RouteValueDictionary {  
+                { "id", id },  
+                { "saveChangesError", true } });
+            }
+            return RedirectToAction("Index"); 
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ContosoUniversity/Global.asax.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Data.Entity;
+using ContosoUniversity.DAL;
+using ContosoUniversity.Models;
+using System.Data.Entity.Infrastructure;
+
+namespace ContosoUniversity
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+
+        }
+
+        protected void Application_Start()
+        {
+            Database.SetInitializer<SchoolContext>(new SchoolInitializer());
+            //SchoolContext context = new SchoolContext();
+            //var students = new List<Student> 
+            //{ 
+            //    new Student { FirstMidName = "Carson",   LastName = "Alexander", EnrollmentDate = DateTime.Parse("2005-09-01") }, 
+            //    new Student { FirstMidName = "Meredith", LastName = "Alonso",    EnrollmentDate = DateTime.Parse("2002-09-01") }, 
+            //    new Student { FirstMidName = "Arturo",   LastName = "Anand",     EnrollmentDate = DateTime.Parse("2003-09-01") }, 
+            //    new Student { FirstMidName = "Gytis",    LastName = "Barzdukas", EnrollmentDate = DateTime.Parse("2002-09-01") }, 
+            //    new Student { FirstMidName = "Yan",      LastName = "Li",        EnrollmentDate = DateTime.Parse("2002-09-01") }, 
+            //    new Student { FirstMidName = "Peggy",    LastName = "Justice",   EnrollmentDate = DateTime.Parse("2001-09-01") }, 
+            //    new Student { FirstMidName = "Laura",    LastName = "Norman",    EnrollmentDate = DateTime.Parse("2003-09-01") }, 
+            //    new Student { FirstMidName = "Nino",     LastName = "Olivetto",  EnrollmentDate = DateTime.Parse("2005-09-01") } 
+            //};
+            //students.ForEach(s => context.Students.Add(s));
+            //context.SaveChanges();
+
+            //var courses = new List<Course> 
+            //{ 
+            //    new Course { Title = "Chemistry",      Credits = 3, }, 
+            //    new Course { Title = "Microeconomics", Credits = 3, }, 
+            //    new Course { Title = "Macroeconomics", Credits = 3, }, 
+            //    new Course { Title = "Calculus",       Credits = 4, }, 
+            //    new Course { Title = "Trigonometry",   Credits = 4, }, 
+            //    new Course { Title = "Composition",    Credits = 3, }, 
+            //    new Course { Title = "Literature",     Credits = 4, } 
+            //};
+            //courses.ForEach(s => context.Courses.Add(s));
+            //context.SaveChanges();
+
+            //var enrollments = new List<Enrollment> 
+            //{ 
+            //    new Enrollment { StudentID = 1, CourseID = 1, Grade = 1 }, 
+            //    new Enrollment { StudentID = 1, CourseID = 2, Grade = 3 }, 
+            //    new Enrollment { StudentID = 1, CourseID = 3, Grade = 1 }, 
+            //    new Enrollment { StudentID = 2, CourseID = 4, Grade = 2 }, 
+            //    new Enrollment { StudentID = 2, CourseID = 5, Grade = 4 }, 
+            //    new Enrollment { StudentID = 2, CourseID = 6, Grade = 4 }, 
+            //    new Enrollment { StudentID = 3, CourseID = 1            }, 
+            //    new Enrollment { StudentID = 4, CourseID = 1,           }, 
+            //    new Enrollment { StudentID = 4, CourseID = 2, Grade = 4 }, 
+            //    new Enrollment { StudentID = 5, CourseID = 3, Grade = 3 }, 
+            //    new Enrollment { StudentID = 6, CourseID = 4            }, 
+            //    new Enrollment { StudentID = 7, CourseID = 5, Grade = 2 }, 
+            //};
+            //enrollments.ForEach(s => context.Enrollments.Add(s));
+            //context.SaveChanges();
+
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
